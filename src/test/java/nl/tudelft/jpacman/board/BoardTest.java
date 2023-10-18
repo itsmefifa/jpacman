7fac71a9b0bf35d9ff94857f33e85cd93a257c2a
itsmefifa <chanisara.kot@student.mahidol.ac.th>
2023-10-18 18:55:53 +0700

    jpacman Assertions

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -20,11 +20,24 @@ public class BoardTest {
         assertThat(board.getWidth()).isEqualTo(1);
         assertThat(board.getHeight()).isEqualTo(1);
     }
+
+    @Test
+    void squareAtReturnsCorrectSquare() {
+        assertThat(board.squareAt(0, 0)).isInstanceOf(BasicSquare.class);
+    }
+
+    @Test
+    void invariantHolds() {
+        assertThat(board.invariant()).isTrue();
+    }
+
     @Test
     void squareAtReturnsNullSquare() {
+        Square s1 = new BasicSquare();
         Square[][] grid = new Square[1][1];
-        grid[0][0] = null;
-        board = new Board(grid);
-        assertThat(board.squareAt(0, 0)).isNull();
+        grid[0][0] = s1;
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0,0)).isEqualTo(grid[0][0]);
     }
+
 }
