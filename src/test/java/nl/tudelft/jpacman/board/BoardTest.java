8f0e49a742179fe06055200c9905e0eaea1ff2f7
Chanisara Kotrachai <82084392+itsmefifa@users.noreply.github.com>
2023-11-02 14:23:45 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -3,34 +3,46 @@ package nl.tudelft.jpacman.board;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-
+ /**
+     * This is the main method.
+     */
 public class BoardTest {
 
     private Board board;
-
+ /**
+     * This is the main method.
+     */
     @BeforeEach
     void setUp() {
         Square[][] grid = new Square[1][1];
         grid[0][0] = new BasicSquare();
         board = new Board(grid);
     }
-
+ /**
+     * This is the main method.
+     */
     @Test
     void verifyBoardSize() {
         assertThat(board.getWidth()).isEqualTo(1);
         assertThat(board.getHeight()).isEqualTo(1);
     }
-
+ /**
+     * This is the main method.
+     */
     @Test
     void squareAtReturnsCorrectSquare() {
         assertThat(board.squareAt(0, 0)).isInstanceOf(BasicSquare.class);
     }
-
+ /**
+     * This is the main method.
+     */
     @Test
     void invariantHolds() {
         assertThat(board.invariant()).isTrue();
     }
-
+ /**
+     * This is the main method.
+     */
     @Test
     void squareAtReturnsNullSquare() {
         Square s1 = new BasicSquare();
