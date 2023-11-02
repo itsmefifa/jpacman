b7fa6fa60696755d4c45965733d52976ec4c8a6b
Chanisara Kotrachai <82084392+itsmefifa@users.noreply.github.com>
2023-11-02 14:24:45 +0700

    Update DirectionTest.java

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -19,16 +19,25 @@ public class DirectionTest {
         Direction north = Direction.valueOf("NORTH");
         assertThat(north.getDeltaY()).isEqualTo(-1);
     }
+     /**
+     * This is the main method.
+     */
     @Test
     void testWest() {
         Direction west = Direction.valueOf("WEST");
         assertThat(west.getDeltaX()).isEqualTo(-1);
     }
+     /**
+     * This is the main method.
+     */
     @Test
     void testEast() {
         Direction east = Direction.valueOf("EAST");
         assertThat(east.getDeltaX()).isEqualTo(1);
     }
+     /**
+     * This is the main method.
+     */
     @Test
     void testSouth() {
         Direction south = Direction.valueOf("SOUTH");
